91354e8feee5ed5f90b6574e52f48aebbe3962eb
Kevin Joseph <kevinjoseph1995@gmail.com>
2022-06-18 07:32:50 +0000

    Remove some cruft

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,24 @@ use std::{
 
 mod ast_printer;
 mod error;
-mod global_handle;
 mod interpreter;
 mod parser;
 mod scanner;
 mod tokens;
 
 use error::LoxError;
-use global_handle::GlobalHandle;
+use interpreter::Interpreter;
 use parser::Parser;
 use scanner::scan_tokens;
 
 fn main() -> Result<(), LoxError> {
     let args: Vec<_> = env::args().collect();
 
-    let mut global_handle = GlobalHandle::new();
+    let mut interpreter = Interpreter::new();
 
     match args.len() {
-        1 => run_from_console(&mut global_handle)?,
-        2 => run_from_file(&args[1], &mut global_handle)?,
+        1 => run_from_console(&mut interpreter)?,
+        2 => run_from_file(&args[1], &mut interpreter)?,
         _ => print_usage(),
     };
 
@@ -38,7 +37,7 @@ fn print_usage() {
     println!("{}", help_message);
 }
 
-fn run_from_console(global_handle: &mut GlobalHandle) -> Result<(), LoxError> {
+fn run_from_console(interpreter: &mut Interpreter) -> Result<(), LoxError> {
     let mut buffer = String::new();
     loop {
         buffer.clear();
@@ -52,21 +51,21 @@ fn run_from_console(global_handle: &mut GlobalHandle) -> Result<(), LoxError> {
             print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
             continue;
         }
-        run(&buffer, global_handle);
+        run(&buffer, interpreter);
     }
     Ok(())
 }
 
-fn run_from_file(filename: &str, global_handle: &mut GlobalHandle) -> Result<(), LoxError> {
+fn run_from_file(filename: &str, interpreter: &mut Interpreter) -> Result<(), LoxError> {
     let mut input_file = std::fs::File::open(filename)?;
     let mut file_contents = String::new();
     input_file.read_to_string(&mut file_contents)?;
-    run(&file_contents, global_handle);
+    run(&file_contents, interpreter);
     Ok(())
 }
 
-fn run(line: &str, global_handle: &mut GlobalHandle) {
-    match run_wrapper(&line, global_handle) {
+fn run(line: &str, interpreter: &mut Interpreter) {
+    match run_wrapper(&line, interpreter) {
         Err(err) => {
             println!("{}", err);
         }
@@ -74,7 +73,7 @@ fn run(line: &str, global_handle: &mut GlobalHandle) {
     }
 }
 
-fn run_wrapper(line: &str, global_handle: &mut GlobalHandle) -> Result<(), LoxError> {
+fn run_wrapper(line: &str, interpreter: &mut Interpreter) -> Result<(), LoxError> {
     // Scan
     let tokens = scan_tokens(&(line.as_bytes()))?;
     // println!("{:?}", tokens);
@@ -83,7 +82,7 @@ fn run_wrapper(line: &str, global_handle: &mut GlobalHandle) -> Result<(), LoxEr
 
     ast_printer::visualize_program_ast(&program); //  Uncomment me to visualize the AST
 
-    global_handle.interpreter.interpret(program)?;
+    interpreter.interpret(program)?;
 
     Ok(())
 }
